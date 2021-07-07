51ef889b555cbbca1f14f34f7330be9c61fc0018
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-06 18:55:07 -0700

    add html

--- js/scripts.js
+++ js/scripts.js
@@ -39,3 +39,27 @@ Contact.prototype.fullName = function(){
   return this.firstName + " " + this.lastName;
 };
 
+// User Interface Logic
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay){
+  let contactsList= $("ul#contacts");
+  let htmlForContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key){
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+}
+
+$(document).ready(function(){
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  });
+});
